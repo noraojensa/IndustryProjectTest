a79fe659a066536be936304fa9f4a31931c19ba2
audurkatarina <55839279+audurkatarina@users.noreply.github.com>
2022-09-12 10:50:17 +0200

    added dataclass Entry functionality

--- section_history/reqhistory.py
+++ section_history/reqhistory.py
@@ -3,12 +3,45 @@ import os
 from pathlib import Path
 from git.repo import Repo
 import glob
+from datetime import datetime
+from dataclasses import dataclass
+
+@dataclass
+class Entry:
+    file: str
+    commit: str
+    author: str
+    date: datetime
+    text: str
+
+class EntryList(list):
+    def __str__(self) -> str:
+        return '\n\n'.join(self)
+
+    # Default str for list objects:
+    #[the first entry, the next entry, ...]
+
+    # Wanted str for this entry list object
+    # to display it better in the console
+    """the first entry
+
+    the second entry
+
+    ..."""
 
 
-def get_and_format_commit_info(repo, filename, current_section):
-    history = ""
-    history += f"File: {filename} \nCommit: {repo.head.commit.hexsha} \nAuthor: {repo.head.commit.author} <{repo.head.commit.author.email}> \nDate: {repo.head.commit.committed_datetime} \n\n{current_section} \n -------------------------------------------------------------\n\n"
-    return history
+def add_entry_info(repo, filename, current_section):
+    #history = ""
+    #history += f"File: {filename} \nCommit: {repo.head.commit.hexsha} \nAuthor: {repo.head.commit.author} <{repo.head.commit.author.email}> \nDate: {repo.head.commit.committed_datetime} \n\n{current_section} \n -------------------------------------------------------------\n\n"
+    #return history
+    new_entry = Entry(
+        filename, 
+        repo.head.commit.hexsha, 
+        f"{repo.head.commit.author} <{repo.head.commit.author.email}>", 
+        f"{repo.head.commit.committed_datetime}", 
+        current_section)
+    return new_entry
+
 
 def get_history(REQID, repo_path):
 
@@ -17,9 +50,10 @@ def get_history(REQID, repo_path):
     repo.git.checkout("main")
 
     searchString = str(repo_path) + '/*.js' #This should only look at markdown files, in accordance with how reqs are stored at Ericsson
-    previous_section = ""
+    #previous_section = Entry("","","",None,"")
+    #current_section = Entry("","","",None,"")
     current_section = ""
-    history = ""
+    history = []
     num_of_changes = 0
 
     #For every file in directory of repo
@@ -38,9 +72,8 @@ def get_history(REQID, repo_path):
                         break           
 
             #If section has changed from last commit, add current section to output. Counts nr. of changes.
-            if found == True and current_section != previous_section:
-                history += get_and_format_commit_info(repo, filename, current_section)
-                previous_section = current_section
+            if found == True and (history == [] or current_section != history[-1].text):
+                history.append(add_entry_info(repo, filename, current_section))
                 num_of_changes += 1
             #Jump one commit back
             current_section = ""
@@ -51,7 +84,7 @@ def get_history(REQID, repo_path):
 
 if __name__ == "__main__":
     reqid = sys.argv[1] #User input REQID to print history for
-    directory = "C:\\Users\\no\\Documents\\Dummy_project"
-    #directory = "/Users/audurtheodorsdottir/Desktop/chalmers/2022stp1/DAT306/Dummy_project"
+    #directory = "C:\\Users\\no\\Documents\\Dummy_project"
+    directory = "/Users/audurtheodorsdottir/Desktop/chalmers/2022stp1/DAT306/Dummy_project"
     #directory = Path.cwd() THE ONE WE'LL USE IN THE END
     print(get_history(reqid, directory))
