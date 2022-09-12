7f847bdb8446628c516a863fd77c8c4e620c604b
audurkatarina <55839279+audurkatarina@users.noreply.github.com>
2022-09-12 17:58:43 +0200

    output formatted to look like main

--- section_history/reqhistory.py
+++ section_history/reqhistory.py
@@ -14,26 +14,17 @@ class Entry:
     date: datetime
     text: str
 
+    def __str__(self):
+        return f"\nFile: {self.file} \nCommit: {self.commit} \nAuthor: {self.author} \nDate: {self.date} \n\n{self.text}"
+
+
 class EntryList(list):
-    def __str__(self) -> str:
-        return '\n\n'.join(self)
-
-    # Default str for list objects:
-    #[the first entry, the next entry, ...]
-
-    # Wanted str for this entry list object
-    # to display it better in the console
-    """the first entry
-
-    the second entry
-
-    ..."""
+    def __str__(self):
+        separator = '\n' + '-'*95 + '\n'
+        return separator.join(str(x) for x in self)
 
 
 def add_entry_info(repo, filename, current_section):
-    #history = ""
-    #history += f"File: {filename} \nCommit: {repo.head.commit.hexsha} \nAuthor: {repo.head.commit.author} <{repo.head.commit.author.email}> \nDate: {repo.head.commit.committed_datetime} \n\n{current_section} \n -------------------------------------------------------------\n\n"
-    #return history
     new_entry = Entry(
         filename, 
         repo.head.commit.hexsha, 
@@ -50,11 +41,8 @@ def get_history(REQID, repo_path):
     repo.git.checkout("main")
 
     searchString = str(repo_path) + '/*.js' #This should only look at markdown files, in accordance with how reqs are stored at Ericsson
-    #previous_section = Entry("","","",None,"")
-    #current_section = Entry("","","",None,"")
     current_section = ""
     history = []
-    num_of_changes = 0
 
     #For every file in directory of repo
     for filename in glob.glob(searchString):
@@ -74,17 +62,17 @@ def get_history(REQID, repo_path):
             #If section has changed from last commit, add current section to output. Counts nr. of changes.
             if found == True and (history == [] or current_section != history[-1].text):
                 history.append(add_entry_info(repo, filename, current_section))
-                num_of_changes += 1
             #Jump one commit back
             current_section = ""
             repo.git.checkout("HEAD~")
         #Return to newest commit again
         repo.git.checkout("main")
-    return f"\nNumber of changes for {REQID}: {num_of_changes}\n\n {history}"
+    return history
 
 if __name__ == "__main__":
     reqid = sys.argv[1] #User input REQID to print history for
     #directory = "C:\\Users\\no\\Documents\\Dummy_project"
     directory = "/Users/audurtheodorsdottir/Desktop/chalmers/2022stp1/DAT306/Dummy_project"
     #directory = Path.cwd() THE ONE WE'LL USE IN THE END
-    print(get_history(reqid, directory))
+    entries = get_history(reqid, directory)
+    print(EntryList(entries))
